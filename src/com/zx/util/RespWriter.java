723381299fa03ee9d73e7293c53faa4e75cc75fa
ZexiGong <gong.ze@husky.neu.edu>
2020-03-01 16:11:31 +0800

    update write function

--- src/com/zx/util/RespWriter.java
+++ src/com/zx/util/RespWriter.java
@@ -8,24 +8,9 @@ import javax.servlet.http.HttpServletResponse;
 import com.alibaba.fastjson.JSONObject;
 import com.zx.common.Result;
 
-/**
- * @ClassName: RespWriter 
- * @Description: 响应对象输出工具类
- * @author: Mr.T
- * @date: 2020年2月23日 上午10:50:47
- */
+
 public abstract class RespWriter {
 
-	/**
-	 * @Title: writerJson
-	 * @author: Mr.T   
-	 * @date: 2020年2月23日 上午10:51:45 
-	 * @Description: 将result 对象处理为json 进行输出
-	 * @param resp
-	 * @param rs
-	 * @return
-	 * @return: String
-	 */
 	public static String writerJson(HttpServletResponse resp,Result rs) {
 		resp.setCharacterEncoding("UTF-8");
 		resp.setContentType("json/text;charset=UTF-8");
